dd1a89b6e439224959da85770da9b5487bc01a90
chiwei82 <55752357+chiwei82@users.noreply.github.com>
2021-08-16 23:16:29 +0800

    completed version

--- drake/map/server.R
+++ drake/map/server.R
@@ -8,10 +8,11 @@
 #
 
 # Load packages
-library(shiny)
-library(dplyr)
-library(leaflet)
-library(plotly)
+require(shiny)
+require(dplyr)
+require(leaflet)
+require(plotly)
+require(sf)
 
 # Load Data
 readRDS("support/osm_test.rds")-> df_ntp
@@ -20,13 +21,23 @@ readRDS("support/NTP_confirmed_data.rds")->  NTP_confirmed_data
 case = colorspace::sequential_hcl(n=3, h =0 , c = c(200, 0), l = 30, rev = TRUE, power = 1.75)
 grDevices::colorRamp(case) -> case_colorGenerator
 
+NTP_confirmed_data$TOWNNAME %>% table() -> confirmed_by_city
+confirmed_by_city %>% as.data.frame() -> cumulative_city
+cumulative_city$. <- factor(cumulative_city$., levels = unique(cumulative_city$.)[order(cumulative_city$Freq, decreasing = F)])
+
+
+table(NTP_confirmed_data$`發病日.從BO抓取.` ) %>% as.data.frame() -> dailyCases
+dailyCases %>% mutate(
+  Var1 = as.Date(Var1)
+) -> dailyCases
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output){
   
     output$leafletPlot <- leaflet::renderLeaflet({
       leaflet(confirmed) %>% 
         setView(
-          lng = 121.5454, lat= 24.9928, zoom = 10.3
+          lng = 121.5454, lat= 25.05, zoom = 11
         ) %>% addTiles() %>% 
         addMarkers(
           lng = ~lon,
@@ -54,16 +65,75 @@ shinyServer(function(input, output){
                 color="black",
                 size=15
                 )
-          ))
+          )) %>% layout(
+            font=list(
+              family="Taipei Sans TC Beta",
+              size=16
+            ),
+            paper_bgcolor='#cfd8dc',
+            plot_bgcolor='#cfd8dc'
+          )
     })
     
     output$plotHist <- plotly::renderPlotly({
       plot_ly(
-        data = NTP_confirmed_data,
-        x = ~發病日.從BO抓取. ,
-        type = "histogram",
-        stroke = I("white")
-      ) %>% config(displayModeBar = F)
+        data = dailyCases,
+        x = ~Var1 ,
+        y = ~Freq,
+        type = "bar",
+        marker = list(color = ~Freq,
+                      colors = grDevices::colorRamp(case),
+                      line = list(color = I("white"),
+                                  width = 0.5
+                                 )
+                      ),
+        hoverlabel = list(
+          bgcolor="orange",
+          font=list(
+            color="black",
+            size=18
+          ))
+      ) %>% config(displayModeBar = F) %>% layout(
+        title = list(text = "日發病量",y=0.97),
+        xaxis = list(title = ""),
+        yaxis = list(title = ""),
+        font=list(
+          family="Taipei Sans TC Beta",
+          size=16
+        ),
+        paper_bgcolor='#cfd8dc',
+        plot_bgcolor='#cfd8dc'
+      )
+    })
+    
+    output$Cumulative <- plotly::renderPlotly({
+      plot_ly(
+        data = cumulative_city,
+        x = ~.,
+        y = ~Freq,
+        type = "bar",
+        marker = list(color = ~Freq,
+                      colors = grDevices::colorRamp(case),
+                      line = list(color = 'rgb(8,48,107)',
+                                  width = 1.5)),
+        hoverlabel = list(
+          bgcolor="orange",
+          font=list(
+            color="black",
+            size=18
+          ))
+      ) %>% config(displayModeBar = F) %>%
+        layout(
+          title = list(text = "累積發病量(以地區分別)",y=0.97),
+          xaxis = list(title = ""),
+          yaxis = list(title = ""),
+          font=list(
+            family="Taipei Sans TC Beta",
+            size=16
+          ),
+          paper_bgcolor='#cfd8dc',
+          plot_bgcolor='#cfd8dc'
+        )
     })
   
 })
--- drake/map/ui.R
+++ drake/map/ui.R
@@ -13,38 +13,110 @@ library(shinymaterial)
 leaflet <- leaflet::leafletOutput("leafletPlot",width="100%", height="600px")
 choropleth <- plotly::plotlyOutput("plotlyPlot",width="100%",height="600px")
 histplot <- plotly::plotlyOutput("plotHist",width="100%",height="600px")
+Cumulative <- plotly::plotlyOutput("Cumulative",width="100%",height="600px")
 
 ui <- material_page(
   title = "Covid-19 Dashboard",
-  nav_bar_fixed = TRUE,
+  nav_bar_fixed = F,
   nav_bar_color = "orange darken-3",
   font_color = "black",
-  background_color="orange lighten-5",
+  background_color="blue-grey lighten-4",
   # Place side-nav in the beginning of the UI
   material_side_nav(
-    fixed = TRUE,
+    fixed = T,
     # Place side-nav tabs within side-nav
     material_side_nav_tabs(
       side_nav_tabs = c(
-        "Location" = "side_nav_tab_1",
-        "Choropleth" = "side_nav_tab_2",
+        "Location (Quarantined)" = "side_nav_tab_1",
+        "Choropleth (Quarantined)" = "side_nav_tab_2",
+        "Cumulative (Sorted by city)" = "side_nav_tab_4",
         "Timeseries" = "side_nav_tab_3"
       ),
-      icons = c("map", "insert_chart","show_chart")
+      icons = c("map", "insert_chart","show_chart","location_city")
     )
   ),
   # Define side-nav tab content
   material_side_nav_tab_content(
     side_nav_tab_id = "side_nav_tab_1",
+    tags$table(
+      tags$thead(
+        tags$tr(
+          tags$th("地區"),
+          tags$th("類別"),
+          tags$th("數量")
+        )
+      ),
+      tags$tbody(
+        tags$tr(
+          tags$td(tags$div(class="chip",tags$div(class="chip","新北市"))),
+          tags$td(tags$div(class="chip",tags$div(class="chip","未解除隔離患者"))),
+          tags$td("1935位 (僅包含地址填寫正確之數量)"
+                  )
+        )
+      )
+    ),
     leaflet
   ),
   material_side_nav_tab_content(
     side_nav_tab_id = "side_nav_tab_2",
-    choropleth
+    tags$table(
+      tags$thead(
+        tags$tr(
+          tags$th("地區"),
+          tags$th("類別"),
+          tags$th("數量")
+        )
+      ),
+      tags$tbody(
+        tags$tr(
+          tags$td(tags$div(class="chip",tags$div(class="chip","新北市"))),
+          tags$td(tags$div(class="chip",tags$div(class="chip","未解除隔離患者"))),
+          tags$td("1935位 (僅包含地址填寫正確之數量)")
+        )
+      )
+    ),
+    tags$div(class="container",
+    choropleth)
   ),
   material_side_nav_tab_content(
     side_nav_tab_id = "side_nav_tab_3",
+    tags$table(
+      tags$thead(
+        tags$tr(
+          tags$th("地區"),
+          tags$th("類別"),
+          tags$th("數量")
+        )
+      ),
+      tags$tbody(
+        tags$tr(
+          tags$td(tags$div(class="chip",tags$div(class="chip","新北市"))),
+          tags$td(tags$div(class="chip",tags$div(class="chip","累積發病患者"))),
+          tags$td("6315位")
+        )
+      )
+    ),
     histplot
+  ),
+  material_side_nav_tab_content(
+    side_nav_tab_id = "side_nav_tab_4",
+    tags$table(
+      tags$thead(
+        tags$tr(
+          tags$th("地區"),
+          tags$th("類別"),
+          tags$th("數量")
+        )
+      ),
+      tags$tbody(
+        tags$tr(
+          tags$td(tags$div(class="chip",tags$div(class="chip","新北市"))),
+          tags$td(tags$div(class="chip",tags$div(class="chip","累積發病患者"))),
+          tags$td("6315位")
+        )
+      )
+    ),
+    Cumulative
   )
 )
 
